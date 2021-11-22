f4961115e9068b9bbb3e1e9de11542ff67cb44a8
Waranchit Chaiwong <waranchitchaiwong@Waranchits-MacBook-Pro.local>
2021-11-22 11:46:39 +0700

    setup linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, {FC} from 'react';
 
 const App: FC = () => {
-  return (
-      <div>
-        <h1>Hello React bookstore</h1>
-      </div>
-  )
+    return (
+        <div>
+            <h1>Hello React bookstore</h1>
+        </div>
+    )
 }
 
 export default App;
