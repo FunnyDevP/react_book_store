4d9a9a412b894d7d77d3943bafe9217e47956093
Waranchit Chaiwong <waranchitchaiwong@Waranchits-MacBook-Pro.local>
2021-11-22 22:12:00 +0700

    draft home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React, { FC } from "react";
 import Header from "./components/Header/Header";
+import { Grid } from "@mui/material";
+import Typography from "@mui/material/Typography";
 
 const App: FC = () => {
   return (
     <>
       <Header data-testid={"app-header"} />
+      <Grid container>
+        <Grid item xs={1} style={{ backgroundColor: "white" }}></Grid>
+        <Grid item xs={7}>
+          <Typography variant={"h5"}>Category 1</Typography>
+        </Grid>
+        <Grid item xs={4} style={{ backgroundColor: "blue" }}></Grid>
+      </Grid>
     </>
   );
 };
